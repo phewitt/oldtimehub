42003df87f1e40f91800169214359fa2a4d963a8
Parker Hewitt <Hewittparker@gmail.com>
2018-01-29 18:17:13 -0700

    add limit to not murder resources

--- src/app/services/tune.service.ts
+++ src/app/services/tune.service.ts
@@ -14,7 +14,7 @@ export class TuneService {
   constructor(private readonly afs: AngularFirestore) {}
 
   getTunes(): Observable<Tune[]> {
-    let tunesCollectionRef = this.afs.collection<Tune>("tunes");
+    let tunesCollectionRef = this.afs.collection<Tune>("tunes", ref => ref.limit(50));
     return tunesCollectionRef.snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data() as Tune;
@@ -23,7 +23,7 @@ export class TuneService {
       });
     });
   }
-  
+
   getTune(id: string): Observable<Tune> {
     let tuneDoc = this.afs.doc<Tune>("tunes/" + id);
     return tuneDoc.valueChanges();
